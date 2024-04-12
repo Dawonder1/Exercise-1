f23c4eb2f2330e0d0533f6b999c9592591882b08
Omotoso Samuel Oluwadarasimi <omotoso.samuel@stu.cu.edu.ng>
2024-04-12 23:02:13 +0100

    project upgraded
    project upgraded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
 
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
     private float speed = 11f;
     private float turnSpeed = 25f;
+    [SerializeField] float horsePower;
     private float horizontalInput;
     private float verticalInput;
-
+    Rigidbody playerRb;
+    [SerializeField] GameObject CenterOfMass;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = CenterOfMass.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
         //Moves the car forward based on vertical input
-        transform.Translate(Vector3.forward *Time.deltaTime * speed * verticalInput);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
 
         //rotates the car based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
